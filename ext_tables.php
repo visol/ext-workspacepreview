eb13a2a2cc70601072acf79d364800b9a0487fe1
lorenzulrich <lulrich@hispeed.ch>
2014-11-24 10:52:25 +0100

    [TASK] Hide WorkspacePreview module

--- ext_tables.php
+++ ext_tables.php
@@ -20,6 +20,14 @@ if (TYPO3_MODE === 'BE' && !(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_INSTALL)) {
 		)
 	);
 
+	// Default User TSConfig to be added in any case.
+	TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
+
+		# Hide the module in the BE.
+		options.hideModules.web := addToList(WorkspacepreviewWorkspacepreview)
+
+	');
+
 	// register ExtDirect
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerExtDirectComponent(
 		'TYPO3.Workspaces.ExtDirect',
