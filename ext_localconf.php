1eec9066ae6d1cf4a3f4185e4129aad819d6a573
lorenzulrich <lulrich@hispeed.ch>
2015-04-01 16:21:02 +0200

    [TASK] Override some language labels from Core and Workspaces (refs #1256)

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,10 +1,12 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-//$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass']['workspaces'] = 'TYPO3\\CMS\\Workspaces\\Hook\\DataHandlerHook';
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_befunc.php']['viewOnClickClass']['workspaces'] = 'Visol\\Workspacepreview\\Hook\\BackendUtilityHook';
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['hook_eofe']['workspaces'] = 'Visol\\Workspacepreview\\Hook\\TypoScriptFrontendControllerHook->hook_eofe';
-//$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/alt_doc.php']['makeEditForm_accessCheck']['workspaces'] = 'TYPO3\\CMS\\Workspaces\\Hook\\BackendUtilityHook->makeEditForm_accessCheck';
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\CMS\Workspaces\Service\StagesService'] =
-	array('className' => 'Visol\Workspacepreview\Xclass\StagesService');
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\CMS\Workspaces\Service\StagesService'] = array('className' => 'Visol\Workspacepreview\Xclass\StagesService');
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:workspaces/Resources/Private/Language/locallang.xlf'][] = 'EXT:workspacepreview/Resources/Private/Language/locallang-override.xlf';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:lang/locallang_mod_user_ws.xlf'][] = 'EXT:workspacepreview/Resources/Private/Language/locallang-override.xlf';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['de']['EXT:workspaces/Resources/Private/Language/locallang.xlf'][] = 'EXT:workspacepreview/Resources/Private/Language/de.locallang-override.xlf';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['de']['EXT:lang/locallang_mod_user_ws.xlf'][] = 'EXT:workspacepreview/Resources/Private/Language/de.locallang-override.xlf';
