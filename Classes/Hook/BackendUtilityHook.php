1d5c1ee2d57257f048a9cff1ac3a8c64cf5ace49
lorenzulrich <lulrich@hispeed.ch>
2015-05-08 18:46:33 +0200

    [TASK] Remove "Go to workspace module" link from Toolbar Items (refs #1102)

--- Classes/Hook/BackendUtilityHook.php
+++ Classes/Hook/BackendUtilityHook.php
@@ -1,5 +1,5 @@
 <?php
-namespace Visol\WorkspacePreview\Hook;
+namespace Visol\Workspacepreview\Hook;
 
 /**
  * This file is part of the TYPO3 CMS project.
--- ext_localconf.php
+++ ext_localconf.php
@@ -5,6 +5,7 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_befunc.php']['viewO
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['hook_eofe']['workspaces'] = 'Visol\\Workspacepreview\\Hook\\TypoScriptFrontendControllerHook->hook_eofe';
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\CMS\Workspaces\Service\StagesService'] = array('className' => 'Visol\Workspacepreview\Xclass\StagesService');
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\CMS\Workspaces\ExtDirect\WorkspaceSelectorToolbarItem'] = array('className' => 'Visol\Workspacepreview\Xclass\ExtDirect\WorkspaceSelectorToolbarItem');
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:workspaces/Resources/Private/Language/locallang.xlf'][] = 'EXT:workspacepreview/Resources/Private/Language/locallang-override.xlf';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:lang/locallang_mod_user_ws.xlf'][] = 'EXT:workspacepreview/Resources/Private/Language/locallang-override.xlf';
