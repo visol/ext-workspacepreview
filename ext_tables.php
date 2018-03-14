888027ed66bacda2e73d235d4a4ce344c8c64614
Jonas Renggli <jonas.renggli@visol.ch>
2018-03-14 19:01:59 +0100

    [FEATURE] Hide Workspaces module in Backend

--- ext_tables.php
+++ ext_tables.php
@@ -1,2 +1,12 @@
 <?php
 defined('TYPO3_MODE') or die();
+
+
+if (TYPO3_MODE === 'BE' && !(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_INSTALL)) {
+
+    // Default User TSConfig to be added in any case.
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
+		# Hide the module in the BE.
+		options.hideModules.web := addToList(WorkspacesWorkspaces)
+	');
+}
